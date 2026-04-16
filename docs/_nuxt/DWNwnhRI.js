32b283cf4b58bdc28cf3665293d12e064c59aedc
Namkhaeng <suchanat.k@clicknext.co.th>
2026-04-16 20:21:53 +0700

    Deploy: Preview Image upload #1

---
+++ docs/_nuxt/DWNwnhRI.js
@@ -0,0 +1,1 @@
+import{_ as e,A as o,z as r}from"./BcE_Cy71.js";const a={};function c(n,t){return r(),o("div",null," For Karaoke stream widget coming soon! ")}const _=e(a,[["render",c]]);export{_ as default};
