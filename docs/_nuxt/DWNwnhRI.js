51004ad4bfc477eda05d825fd75302905b50b424
Namkhaeng <suchanat.k@clicknext.co.th>
2026-04-16 20:42:01 +0700

    Fix: Badge Bug When Deply

--- docs/_nuxt/DWNwnhRI.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,A as o,z as r}from"./BcE_Cy71.js";const a={};function c(n,t){return r(),o("div",null," For Karaoke stream widget coming soon! ")}const _=e(a,[["render",c]]);export{_ as default};
